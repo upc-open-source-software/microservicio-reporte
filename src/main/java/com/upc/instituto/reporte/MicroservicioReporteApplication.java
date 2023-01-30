853fa594b0ef210cd9330ffc302328426ee33f6a
Luis Guisado <lguisadom@gmail.com>
2023-01-30 16:54:42 -0500

    First commit

---
+++ src/main/java/com/upc/instituto/reporte/MicroservicioReporteApplication.java
@@ -0,0 +1,13 @@
+package com.upc.instituto.reporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioReporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioReporteApplication.class, args);
+	}
+
+}
